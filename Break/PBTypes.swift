fcdd6c8ec01eb5ef66a557b2998576697c735150
Armin Hamar <arminhamar@macbook-pro.local>
2017-02-25 12:05:05 +0100

    A File for Type alias declarations

---
+++ Break/PBTypes.swift
@@ -0,0 +1,21 @@
+//
+//  PBTypes.swift
+//  Break
+//
+//  Created by Armin Hamar on 25.02.17.
+//  Copyright © 2017 ProjektBlue. All rights reserved.
+//
+
+import Foundation
+
+typealias i8 = Int8
+typealias i16 = Int16
+typealias i32 = Int32
+typealias i64 = Int64
+
+typealias str = String
+
+typealias f32 = Float32
+typealias f64 = Float64
+
+typealias int = Int
