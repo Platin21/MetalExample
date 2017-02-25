c0c1c3cb62a957af951d571a443ef52bcbe0172b
Armin Hamar <arminhamar@macbook-pro.local>
2017-02-25 11:44:08 +0100

    Removed moved code and also delete touchesBegan !

--- Break/ViewController.swift
+++ Break/ViewController.swift
@@ -26,53 +26,12 @@ class ViewController: UIViewController
         super.viewDidLoad()
         
         mtlview.device = MTLCreateSystemDefaultDevice();
-        paRender = PARender(device: mtlview.device!)
+        paRender = PARender(device: mtlview.device!,view:mtlview)
         mtlview.delegate = paRender;
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        /*
-        for touch in touches
-        {
-            print(touch.location(in: self.mtlview))
-        }
-        */
-    }
-    
-    
-    
 }
-
-/*
-extension ViewController : MTKViewDelegate
-{
-    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-        
-        
-        
-    }
-    
-    func draw(in view: MTKView) {
-        
-        guard let drawable = view.currentDrawable,let desc = view.currentRenderPassDescriptor
-        else
-        {
-            return
-        }
-        
-        let cmdBuffer = cmdQueue.makeCommandBuffer()
-        let cmdEncoder = cmdBuffer.makeRenderCommandEncoder(descriptor: desc)
-        
-        cmdEncoder.endEncoding()
-        cmdBuffer.present(drawable)
-        cmdBuffer.commit()
-        
-        
-    }
-}
-*/
-
 
 
 
